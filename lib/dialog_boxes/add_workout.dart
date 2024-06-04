74514da8075fec5e26385a7b81569da7c7029cef
Anshu-79 <tanwaranshumaan@gmail.com>
2024-06-04 22:07:24 +0530

    design workout input dialog

--- lib/dialog_boxes/add_workout.dart
+++ lib/dialog_boxes/add_workout.dart
@@ -3,6 +3,7 @@ import 'package:searchable_listview/searchable_listview.dart';
 
 import 'package:aqua/database/database.dart';
 import 'package:aqua/utils.dart' as utils;
+import 'package:aqua/dialog_boxes/customize_workout.dart';
 
 class AddWorkoutDialog extends StatefulWidget {
   const AddWorkoutDialog({super.key, required this.notifyParent});
@@ -29,80 +30,104 @@ class _AddWorkoutDialogState extends State<AddWorkoutDialog> {
 
   @override
   Widget build(BuildContext context) {
-    return Dialog.fullscreen(
-      backgroundColor: Theme.of(context).canvasColor,
-      child: Padding(
-        padding: const EdgeInsets.only(top: 40, right: 10, left: 10),
-        child: SearchableList<Activity>.async(
-          spaceBetweenSearchAndList: 5,
-          displaySearchIcon: false,
-          cursorColor: Theme.of(context).primaryColor,
-          closeKeyboardWhenScrolling: true,
-          searchFieldEnabled: true,
-          asyncListCallback: () async {
-            final Future<List<Activity>> activities = _db.getActivities();
-            return activities;
-          },
-          asyncListFilter: (q, list) {
-            return list
-                .where((element) => element.description.contains(q))
-                .toList();
-          },
-          itemBuilder: (Activity activity) => ActivityItem(
-            activity: activity,
-          ),
-          emptyWidget: const Center(
-            child: Column(
+    return Scaffold(
+      body: Dialog.fullscreen(
+        backgroundColor: Theme.of(context).canvasColor,
+        child: Padding(
+          padding: const EdgeInsets.only(top: 40, right: 10, left: 10),
+          child: SearchableList<Activity>.async(
+            spaceBetweenSearchAndList: 5,
+            displaySearchIcon: false,
+            cursorColor: Theme.of(context).primaryColor,
+            closeKeyboardWhenScrolling: true,
+            searchFieldEnabled: true,
+            asyncListCallback: () async {
+              final Future<List<Activity>> activities = _db.getActivities();
+              return activities;
+            },
+            asyncListFilter: (q, list) {
+              return list
+                  .where((element) => element.description.contains(q))
+                  .toList();
+            },
+            itemBuilder: (Activity activity) => ActivityItem(
+              activity: activity,
+              database: _db,
+            ),
+            emptyWidget: const Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  // TODO: Replace with mascot
+                  Icon(
+                    Icons.sentiment_dissatisfied_outlined,
+                    size: 60,
+                  ),
+                  Text("Nothing in here!"),
+                ],
+              ),
+            ),
+            onRefresh: () async {},
+            onItemSelected: (Activity item) {},
+            inputDecoration: InputDecoration(
+              prefixIcon: const Icon(Icons.search),
+              floatingLabelStyle:
+                  TextStyle(color: Theme.of(context).primaryColor),
+              labelStyle: utils.ThemeText.searchLabelText,
+              labelText: "Search Activity",
+              fillColor: Colors.white,
+              focusedBorder: OutlineInputBorder(
+                borderSide: const BorderSide(
+                  color: Colors.blue,
+                  width: 1.0,
+                ),
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+            ),
+            errorWidget: const Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                // TODO: Replace with mascot
                 Icon(
-                  Icons.sentiment_dissatisfied_outlined,
-                  size: 60,
+                  Icons.error,
+                  color: Colors.red,
                 ),
-                Text("Nothing in here!"),
+                SizedBox(
+                  height: 20,
+                ),
+                Text('Error while fetching actors')
               ],
             ),
           ),
-          onRefresh: () async {},
-          onItemSelected: (Activity item) {},
-          inputDecoration: InputDecoration(
-            prefixIcon: const Icon(Icons.search),
-            floatingLabelStyle:
-                TextStyle(color: Theme.of(context).primaryColor),
-            labelStyle: utils.ThemeText.searchLabelText,
-            labelText: "Search Activity",
-            fillColor: Colors.white,
-            focusedBorder: OutlineInputBorder(
-              borderSide: const BorderSide(
-                color: Colors.blue,
-                width: 1.0,
-              ),
-              borderRadius: BorderRadius.circular(10.0),
-            ),
-          ),
-          errorWidget: const Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Icon(
-                Icons.error,
-                color: Colors.red,
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Text('Error while fetching actors')
-            ],
+        ),
+      ),
+      floatingActionButton: SizedBox(
+        height: 70,
+        width: 70,
+        child: FloatingActionButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          tooltip: "Exit",
+          backgroundColor: Theme.of(context).primaryColor,
+          splashColor: Theme.of(context).splashColor,
+          shape: const CircleBorder(eccentricity: 0),
+          child: Icon(
+            Icons.close,
+            color: Theme.of(context).canvasColor,
+            size: 50,
           ),
         ),
       ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
 
 class ActivityItem extends StatelessWidget {
-  const ActivityItem({super.key, required this.activity});
+  const ActivityItem(
+      {super.key, required this.activity, required this.database});
   final Activity activity;
+  final Database database;
 
   @override
   Widget build(BuildContext context) {
@@ -116,7 +141,25 @@ class ActivityItem extends StatelessWidget {
       titleTextStyle: utils.ThemeText.listTileTitle,
       subtitle: Text(activity.description),
       isThreeLine: true,
-      onTap: () {},
+      onTap: () async {
+        WorkoutsCompanion? addedWorkout = await showGeneralDialog(
+            barrierDismissible: false,
+            transitionDuration: const Duration(milliseconds: 150),
+            transitionBuilder: (context, a1, a2, child) {
+              return ScaleTransition(
+                  scale: Tween<double>(begin: 0.5, end: 1.0).animate(a1),
+                  child: FadeTransition(
+                    opacity: Tween<double>(begin: 0.5, end: 1.0).animate(a1),
+                    child: CustomizeWorkout(
+                      activity: activity,
+                    ),
+                  ));
+            },
+            context: context,
+            pageBuilder: (context, a1, a2) {
+              return const Placeholder();
+            });
+      },
     );
   }
 }
---
+++ lib/dialog_boxes/customize_workout.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+import 'package:drift/drift.dart' as drift;
+import 'package:numberpicker/numberpicker.dart';
+
+import 'package:aqua/utils.dart' as utils;
+import 'package:aqua/database/database.dart';
+
+class CustomizeWorkout extends StatefulWidget {
+  const CustomizeWorkout({super.key, required this.activity});
+  final Activity? activity;
+
+  @override
+  State<CustomizeWorkout> createState() => _CustomizeWorkoutState();
+}
+
+class _CustomizeWorkoutState extends State<CustomizeWorkout> {
+  int _duration = 20;
+
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      backgroundColor: Theme.of(context).canvasColor,
+      shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(40),
+          side: BorderSide(width: 5, color: Theme.of(context).primaryColor)),
+      surfaceTintColor: Colors.transparent,
+      child: Container(
+        height: 475,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Icon(
+                utils.getWorkoutIcon(widget.activity!.activityID),
+                size: 60,
+              ),
+              FittedBox(
+                  fit: BoxFit.contain,
+                  child: Text(
+                    widget.activity!.category,
+                    style: utils.ThemeText.workoutTitle,
+                  )),
+              Text(
+                widget.activity!.description,
+                maxLines: 2,
+                overflow: TextOverflow.ellipsis,
+                textAlign: TextAlign.center,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  NumberPicker(
+                      itemHeight: 60,
+                      itemWidth: 120,
+                      selectedTextStyle: TextStyle(
+                          color: Theme.of(context).primaryColor,
+                          fontWeight: FontWeight.w900,
+                          fontSize: 50),
+                      textStyle: TextStyle(
+                          color: Theme.of(context).primaryColor,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20),
+                      axis: Axis.vertical,
+                      minValue: 10,
+                      step: 5,
+                      maxValue: 300,
+                      value: _duration,
+                      onChanged: (value) => setState(() => _duration = value)),
+                  FittedBox(
+                    fit: BoxFit.contain,
+                    child: Text(
+                      "minutes",
+                      style: utils.ThemeText.durationSubtext,
+                    ),
+                  )
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  utils.addDrinkDialogButtons(
+                    icon: const Icon(Icons.check),
+                    function: () {
+                      final workout = WorkoutsCompanion(
+                        activityID: drift.Value(widget.activity!.activityID),
+                        datetime: drift.Value(DateTime.now()),
+                        duration: drift.Value(_duration),
+                      );
+                      Navigator.pop(context, workout);
+                    },
+                  ),
+                  utils.addDrinkDialogButtons(
+                      icon: const Icon(Icons.close),
+                      function: () =>
+                          Navigator.of(context, rootNavigator: true).pop())
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
