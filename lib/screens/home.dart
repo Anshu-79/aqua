7d2bef45895e9965922a91eb19844bb24170affa
Anshu-79 <tanwaranshumaan@gmail.com>
2024-06-24 21:11:45 +0530

    implement primitive circular fab

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,4 +1,5 @@
 import 'package:aqua/dialog_boxes/add_drink.dart';
+import 'package:drift/drift.dart' as drift;
 import 'package:fab_circular_menu_plus/fab_circular_menu_plus.dart';
 import 'package:flutter/material.dart';
 
@@ -68,17 +69,47 @@ class _HomeScreenState extends State<HomeScreen> {
         fabChild: GestureDetector(
             child: const Icon(Icons.add, size: 30),
             onLongPress: () async {
+              List<Beverage> beverages = await widget.database.getBeverages();
               await utils.GlobalNavigator.showAnimatedDialog(AddDrinkDialog(
-                beverages: await widget.database.getBeverages(),
+                beverages: beverages,
                 notifyParent: refresh,
               ));
             }),
-        children: const [
-          Icon(Icomoon.coffee_cup),
-          Icon(Icomoon.water_glass),
-          Icon(Icomoon.soda_can),
+        children: [
+          getFabButton(
+            widget.database,
+            'Water',
+            const Icon(Icomoon.water_glass),
+          ),
+          getFabButton(
+            widget.database,
+            'Coffee',
+            const Icon(Icomoon.coffee_cup),
+          ),
+          getFabButton(
+            widget.database,
+            'Soda',
+            const Icon(Icomoon.soda_can),
+          )
         ],
       ),
     );
   }
+}
+
+Widget getFabButton(Database db, String name, Icon icon) {
+  return GestureDetector(
+    onTap: () async {
+      Beverage bev = await db.getBeverageFromName(name);
+      DrinksCompanion drink = DrinksCompanion(
+          bevID: drift.Value(bev.bevID),
+          volume: const drift.Value(200), //TODO: Replace with variable value
+          datetime: drift.Value(DateTime.now()));
+      // db.insertOrUpdateDrink(entity)
+    },
+    child: IconButton(
+      icon: icon,
+      onPressed: () {},
+    ),
+  );
 }
